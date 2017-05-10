f2fbd4ebf81cfd6e570bf17cd24e9fd61ca38168
Justin Green <justin98366@gmail.com>
2017-05-09 22:58:57 -0700

    initial commit with some code started

---
+++ scripts/project.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var projects = [];
+
+function Project (projectData){
+  this.name = projectData.name;
+  this.picture = projectData.picture;
+  this.link = projectData.link;
+  this.description = projectData.description;
+}
---
+++ scripts/projectList.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var projectInfo = [{
+  name: 'Title',
+  picture: 'picture image',
+  link: 'project url',
+  description: 'project description'
+}];
