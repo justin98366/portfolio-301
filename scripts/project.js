9966491ee90aef6dac6b9f4bff053d20eeff13be
Justin Green <justin98366@gmail.com>
2017-05-11 23:30:51 -0700

    made data appear on the DOM

--- scripts/project.js
+++ scripts/project.js
@@ -8,3 +8,20 @@ function Project (projectData){
   this.link = projectData.link;
   this.description = projectData.description;
 }
+
+Project.prototype.toHtml = function() {
+  // TODO: Use handlebars to render your articles.
+  //       - Get your template from the DOM.
+  //       - Now "compile" your template with Handlebars.
+  var template = $('#article-template').html();
+  var templateRender = Handlebars.compile(template);
+  return templateRender(this);
+};
+
+projectInfo.forEach(function(articleObject) {
+  projects.push(new Project(articleObject));
+});
+
+projects.forEach(function(article){
+  $('#articles').append(article.toHtml());
+});
